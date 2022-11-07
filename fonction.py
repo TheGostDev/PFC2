a958695a22406d96e4d51c91d78a37e74772a340
Romain B <rboisseau@gaming.tech>
2022-11-07 11:31:52 +0100

    add def

---
+++ fonction.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return(x + y)
+
+def sub(x, y):
+    return(x - y)
+
+def multiply(x, y):
+    return(x * y)
+
+def divide(x, y):
+    return(x / y)
+
+def modulo(x, y):
+    return (x % y)
+
+def salaire(salaireh, hJo, JoA):
+    return(salaireh*hJo,JoA/365/24/60/60)
+
+def salairebrut(net):
+    return(net*1.25)
