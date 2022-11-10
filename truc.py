1a52375f7d42320a46583cb9417b3ff1a184bb9c
Romain <rboisseau@gaming.tech>
2022-11-10 19:22:37 +0100

    updating truc.py

--- truc.py
+++ truc.py
@@ -5,7 +5,7 @@ def morpionBase(x):
     tabF = [] #Initialisation du tableau final
     while i < x: #Fonction tant que i < x
         #Alors, on ajoute 1 valeur de x à tab et on incrémente i de 1
-        tabX = tabX + i
+        tabX = tabX.append(i)
         
         i = i + 1
     tabF = tabX
